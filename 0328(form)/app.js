15b0f609dff44f5cbcde4c70fe0a21e77a7f91e6
8566yu <8566yu@gmail.com>
2023-03-28 20:00:49 +0900

    0328 form get,post

---
+++ 0328(form)/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get("/", function (req, res) {
+  const myTitle = "폼 실습을 합시다~";
+  res.render("index", { title: myTitle });
+});
+
+app.get("/getForm", function (req, res) {
+  console.log(req.query);
+  res.send("get 요청 성공!");
+});
+
+app.post("/postForm", function (req, res) {
+  console.log(req.body);
+  res.send("post 요청 성공!");
+});
+app.listen(PORT, function () {
+  console.log("웹 서버 실행!");
+  console.log(`http://localhost:${PORT}`);
+});
