e53eda0d53a8a5af36e05b0a65a6ca42cd56da2a
8566yu <8566yu@gmail.com>
2023-03-29 16:51:04 +0900

    0329

--- 0328(form)/app.js
+++ 0328(form)/app.js
@@ -13,12 +13,22 @@ app.get("/", function (req, res) {
 
 app.get("/getForm", function (req, res) {
   console.log(req.query);
-  res.send("get 요청 성공!");
+  // res.send("get 요청 성공!");
+  res.render("result", {
+    title: "GET",
+    id: req.query.id,
+    pw: req.query.pw,
+  });
 });
 
 app.post("/postForm", function (req, res) {
   console.log(req.body);
-  res.send("post 요청 성공!");
+  // res.send("post 요청 성공!");
+  res.render("result", {
+    title: "POST",
+    id: req.body.id,
+    pw: req.body.pw,
+  });
 });
 app.listen(PORT, function () {
   console.log("웹 서버 실행!");
