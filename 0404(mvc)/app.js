38859eca31cb1fa8491b2e8843242150ca8436b2
8566yu <8566yu@gmail.com>
2023-04-04 18:26:23 +0900

    0404 mvc++

--- 0404(mvc)/app.js
+++ 0404(mvc)/app.js
@@ -1,16 +1,24 @@
-const express = require("express"); // express 모듈 셋팅
+const express = require("express");
 const app = express();
 const PORT = 8000;
 
 app.set("view engine", "ejs");
-app.use("/views", express.static(__dirname + "/views")); //views 파일설정
-app.use(express.urlencoded({ extended: true })); // post 요청으로 들어오는 모든 형식의 데이터를 파싱(분석)
+app.use("/views", express.static(__dirname + "/views"));
+app.use("/static", express.static(__dirname + "/static"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function (req, res) {
-  res.render("index"); // views/index.ejs 파일을 찾아서 응답
+const indexRouter = require("./routes");
+//require : 폴더까지만 접근할 경우엔, 자동으로 index.js파일을 찾아감.
+app.use("/", indexRouter);
+// '/~~~~~~' 모든 라우터를 /routes/index.js에 정의해 놨으니,
+// /routes/index.js 이 파일을 읽어보면 된다.
+
+app.get("*", (req, res) => {
+  // res.send('404 Error! 잘못된 주소 형식입니다.');
+  res.render("404");
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
