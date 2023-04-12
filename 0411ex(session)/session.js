1e6bc617e30adb0dcc3d673e7a66f54dca32ce81
8566yu <8566yu@gmail.com>
2023-04-12 17:38:10 +0900

    0411 session

--- 0411ex(session)/session.js
+++ 0411ex(session)/session.js
@@ -15,8 +15,6 @@ app.use("/static", express.static(__dirname + "/static"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const userRouter = require("./routes/user");
-app.use("/", userRouter);
 // session({ 세션모듈_옵션 })
 // - secret: 세션을 발급할 때 사용되는 키 (임의로 변조하는 것을 방지)
 // - resave: 세션에 수정사항이 생기지 않더라도 매 요청(request)마다 다시 저장할 건지
@@ -24,55 +22,72 @@ app.use("/", userRouter);
 // - secure: https 프로토콜에서만 세션을 주고 받을 건지
 app.use(
   session({
+    key: "banana",
     secret: process.env.SECRET_KEY, // 필수 옵션 (세션 암호화할 떄 쓰이는 키)
     resave: false,
     saveUninitialized: false, // 일반적으로 false 지정
   })
 );
 
-// 세션 설정
-// req.session.키 = 값
-
-// 세션 읽기(사용)
-// req.session.키
-
-// 세션 삭제
-// req.session.destroy(callback_함수)
+const router = express.Router();
 
 app.get("/", (req, res) => {
   res.render("session");
 });
 
-app.get("/set", (req, res) => {
-  // 세션 설정
-  req.session.name = "홍길동";
-  // 응답
-  res.send("세션 설정 완료!");
+// app.get("/set", (req, res) => {
+//   // 세션 설정
+//   req.session.name = "홍길동";
+//   // 응답
+//   res.send("세션 설정 완료!");
+// });
+
+// app.get("/name", (req, res) => {
+//   // 세션 읽기(사용)
+//   // req.session.키
+//   // req.sessionID: 현재 세션의 ID (서버가 클라이언트를 구별하기 위해 부여하는 고유 값)
+//   // -> 브라우저 단위
+//
+//   res.send({
+//     id: req.sessionID,
+//     name: req.session.name,
+//   });
+// });
+//
+// app.get("/destroy", (req, res) => {
+//   // req.session.destroy(세션 삭제시 호출할 콜백함수)
+//   // -> 세션 완전히 삭제하고, 웹 사이트 접속할 때마다 새로운 sessionID 할당
+//   req.session.destroy((err) => {
+//     if (err) {
+//       throw err;
+//     }
+//
+//     res.send("세션 삭제 완료!");
+//   });
+// });
+
+//http://localhost:3000/process/product 이 주소로 치면 라우터를 통해 바로 여기로 올 수 있다
+router.route("/views").get(function (req, res) {
+  //세션정보는 req.session 에 들어 있다
+  if (req.session.user) {
+    //세션에 유저가 있다면
+    res.redirect("/session");
+  } else {
+    res.redirect("/welcome");
+  }
 });
 
-app.get("/name", (req, res) => {
-  // 세션 읽기(사용)
-  // req.session.키
-  // req.sessionID: 현재 세션의 ID (서버가 클라이언트를 구별하기 위해 부여하는 고유 값)
-  // -> 브라우저 단위
+router.route("/views/session").post(
+  //설정된 쿠키정보를 본다
+  function (req, res) {
+    const paramID = req.body.id || req.query.id;
+    const pw = req.body.pw || req.query.passwords;
+  }
+);
 
-  res.send({
-    id: req.sessionID,
-    name: req.session.name,
-  });
-});
-
-app.get("/destroy", (req, res) => {
-  // req.session.destroy(세션 삭제시 호출할 콜백함수)
-  // -> 세션 완전히 삭제하고, 웹 사이트 접속할 때마다 새로운 sessionID 할당
-  req.session.destroy((err) => {
-    if (err) {
-      throw err;
-    }
-
-    res.send("세션 삭제 완료!");
-  });
-});
+//라우터 미들웨어 등록하는 구간에서는 라우터를 모두  등록한 이후에 다른 것을 세팅한다
+//그렇지 않으면 순서상 라우터 이외에 다른것이 먼저 실행될 수 있다
+app.use("/", router); //라우트 미들웨어를 등록한다
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
