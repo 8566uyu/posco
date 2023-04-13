2b63eefe07cdcbc4b1d7e153d3612e3dcdfa49fc
8566yu <8566yu@gmail.com>
2023-04-14 05:11:54 +0900

    0412,13

---
+++ 0412~(socket)/chat.js
@@ -0,0 +1,158 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const PORT = 8000;
+
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
+app.use("/static", express.static(__dirname + "/static"));
+
+app.get("/", function (req, res) {
+  console.log("client connected");
+  res.render("chat");
+});
+
+//닉네임을 저장할 객체
+// : 닉네임이 겹치지 않도록 객체({ })를 사용함
+// { 소켓_아이디: 닉네임, ...}
+const nickObj = {};
+
+//닉네임 리스크 객체 업데이트
+function updateNickList() {
+  //닉네임 리스트 전송
+  io.emit("updateNicks", nickObj);
+  // 서버에 접속한 클라이언트들에게 nickObj에 변경이 일어났음을 알리는 이벤트
+}
+
+// io.on(event_name, callback)
+// : socket과 관련된 통신 작업 처리
+io.on("connection", (socket) => {
+  // "connection" event
+  // - 클라이언트가 접속했을 때 발생하는 이벤트
+  // - 콜백으로 socket 객체를 제공
+
+  // ⭕❌
+  //최초 입장시 (연결)
+  // socket.id: 소켓 고유 아이디 ->socket 은 웹 페이지별로 id 생성!
+  // 크롬에서 탭 2개 띄우면 socket.id 는 각각 생김 (2개)
+  console.log("⭕ Server Socket Connected >> ", socket.id);
+
+  // [실습1]
+  // socket.on("hello", (data) => {
+  //   console.log(`${data.who}: ${data.msg}`);
+  //   //server -> client 보낼 때
+  //   socket.emit("hellokr", { who: "hello", msg: "안녕 ~" });
+  // });
+  //
+  // socket.on("study", (data) => {
+  //   console.log(`${data.who}: ${data.msg}`);
+  //   //server -> client 보낼 때
+  //   socket.emit("studykr", { who: "study", msg: "공부 좀 혀 ~" });
+  // });
+  //
+  // socket.on("bye", (data) => {
+  //   console.log(`${data.who}: ${data.msg}`);
+  //   //server -> client 보낼 때
+  //   socket.emit("byekr", { who: "bye", msg: "잘 가라 ~" });
+  // });
+
+  //[실습3] 채팅창 입장 안내 문구
+  // io.emit("notice", `${socket.id.slice(0, 5)}님이 입장하셨습니다.`);
+  socket.on("setNick", (nick) => {
+    console.log("socket in setNick >> ", nick); // 프론트에서 입력한 값
+
+    // 닉네임 중복 여부
+    if (Object.values(nickObj).indexOf(nick) > -1) {
+      // 아이디 중복
+      socket.emit("error", "이미 존재하는 닉네임입니다. 다시 입력해주세요!");
+    } else {
+      // 아이디 통과
+      nickObj[socket.id] = nick; //nick 객체에 '소켓_아이디: 닉네임' 추가
+      io.emit("notice", `${nick}님이 입장하셨습니다.`); // 입장 메세지 "전체 공지"
+      // 전체 공지 => 서버에 접속한 모든 클라이언트에게 이벤트 전송
+      socket.emit("entrySuccess", nick); //입장 성공시
+      updateNickList(); // 닉네임 리스크 객체 업데이트
+    }
+  });
+
+  // 실습 3-3 접속자 퇴장
+  //disconnect event : 클라이언트가 연결을 끊었을 떄 발생 (브라우저 탭 닫음)
+  socket.on("disconnect", () => {
+    console.log("**** Server socket Disconnected >> ", socket.id);
+    io.emit("notice", `${nickObj[socket.id]}님이 퇴장하셨습니다.`); // 퇴장 메세지 "전체 공지"
+    // 전체 공지 => 서버에 접속한 모든 클라이언트에게 이벤트 전송
+    delete nickObj[socket.id];
+    updateNickList(); // 닉네임 리스크 객체 업데이트
+  });
+
+  // //실습4 채팅창 메세지 전송
+  // socket.on("send", (obj) => {
+  //   console.log("socket on send >> ", obj);
+  //
+  //   const sendData = {
+  //     nick: obj.myNick,
+  //     msg: obj.msg,
+  //   };
+  //   io.emit("newMessage", sendData);
+  // });
+
+  // 이미지 URL 배열
+  const imageUrls = [
+    "/views/quokka1.png",
+    "/views/quokka2.png",
+    "/views/quokka3.png",
+  ];
+
+  // 클라이언트별 이미지 URL 매핑 객체
+  const clientImageUrls = new Map();
+
+  // 랜덤 이미지 URL 반환하는 함수
+  function getRandomImageUrl(clientId) {
+    let imageUrl;
+    if (clientImageUrls.has(clientId)) {
+      // 이미 클라이언트에게 할당된 이미지 URL이 있는 경우, 해당 URL 반환
+      imageUrl = clientImageUrls.get(clientId);
+    } else {
+      // 클라이언트에게 할당된 이미지 URL이 없는 경우, 랜덤하게 선택하여 매핑 객체에 저장하고 해당 URL 반환
+      const randomIndex = Math.floor(Math.random() * imageUrls.length);
+      imageUrl = imageUrls[randomIndex];
+      clientImageUrls.set(clientId, imageUrl);
+    }
+    return imageUrl;
+  }
+
+  // "send" 이벤트 전송 { 닉네임, 입력메세지 }
+  function send() {
+    const data = {
+      myNick: myNick,
+      msg: document.querySelector("#message").value,
+      imageUrl: getRandomImageUrl(socket.id), // 랜덤 이미지 URL 추가
+    };
+    socket.emit("send", data);
+
+    document.querySelector("#message").value = ""; // 인풋 초기화
+  }
+
+  // 실습4 채팅창 메세지 전송
+  socket.on("send", (obj) => {
+    console.log("socket on send >> ", obj);
+
+    const sendData = {
+      nick: obj.myNick,
+      msg: obj.msg,
+      imageUrl: getRandomImageUrl(socket.id), // 랜덤 이미지 URL 추가
+    };
+    io.emit("newMessage", sendData);
+  });
+
+  // 클라이언트로부터 새 메시지 수신 시 처리
+  socket.on("newMessage", (data) => {
+    console.log("socket on newMessage >> ", data);
+  });
+});
+
+// 주의) socket 을 사용할 때는 http.listen으로 PORT 열어야 함!!!
+http.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
---
+++ 0412~(socket)/static/index.js
@@ -0,0 +1,199 @@
+// frontend js
+
+// socket 사용을 위해서 객체 생성
+let socket = io.connect();
+
+let myNick;
+socket.on("connect", () => {
+  console.log("⭕️ Client Socket Connected >> ", socket.id);
+});
+
+// [실습1]
+// function sayHello() {
+//   // clinet -> server 정보 보내기
+//   // socket.emit(event, data): 데이터 "전송"
+//   // => event 라는 이름으로 data를 전송
+//   socket.emit("hello", { who: "client", msg: "hello" });
+//
+//   //socket.on(event, callback):데이터 "수신"
+//   // event에 대해서 정보를 받아 callback 실행
+//
+//   socket.on("hellokr", (data) => {
+//     const p = document.querySelector("#form-server");
+//     p.textContent = `${data.who}: ${data.msg}`;
+//   });
+// }
+//
+// function sayStudy() {
+//   socket.emit("study", { who: "client", msg: "study" });
+//
+//   socket.on("studykr", (data) => {
+//     const p = document.querySelector("#form-server");
+//     p.textContent = `${data.who}: ${data.msg}`;
+//   });
+// }
+//
+// function sayBye() {
+//   socket.emit("bye", { who: "client", msg: "bye" });
+//
+//   socket.on("byekr", (data) => {
+//     const p = document.querySelector("#form-server");
+//     p.textContent = `${data.who}: ${data.msg}`;
+//   });
+// }
+
+// 실습3 채팅창 입장/퇴장 안내 문구
+// [실습3] 채팅창 입장 안내 문구
+socket.on("notice", (msg) => {
+  document
+    .querySelector("#chatBox")
+    .insertAdjacentHTML("beforeend", `<div class="notice">${msg}</div>`);
+});
+
+//실습 3-2
+function entry() {
+  socket.emit("setNick", document.querySelector("#nickname").value);
+}
+
+socket.on("entrySuccess", (nick) => {
+  // 1. 내 닉네임 설정
+  myNick = nick;
+
+  // 2. 닉네임 입력창 & 버튼 비활성화
+  document.querySelector("#nickname").disabled = true; // 입력창 비활성화 (클릭 막기)
+  document.querySelector(".entry-box > button").disabled = true; // 버튼 비활성화 (클릭 막기)
+
+  // 3. div.chat-box 요소 보이기
+  document.querySelector(".chat-Box").classList.remove("d-none");
+  document.querySelector(".entry-box").style.display = "none";
+  document.querySelector(".my-chat-text").style.display = "none";
+  document.querySelector(".other-chat-text").style.display = "none";
+});
+
+//실습3-2
+//닉네임 중복 -> alert 띄우기
+socket.on("error", (msg) => {
+  alert(msg);
+});
+
+//닉네임 리스트 객체 업데이트하는 이벤트를 받음
+socket.on("updateNicks", (obj) => {
+  let options = `<option value='all'>전체</option>`;
+
+  //select#nick-list 요소의 option 추가
+  for (let key in obj) {
+    options += `<option value='${key}'>${obj[key]}</option>`;
+  }
+
+  //select#nick-list 요소에 options 추가(덮어쓰기)
+  document.querySelector("#nick-list").innerHTML = options;
+});
+
+// 메세지 전송
+// function click_me() {
+//   const message = document.querySelector("#message").value; // 입력된 메세지 값 가져오기
+//   const chatBox = document.querySelector("#chatBox"); // 채팅 박스 요소 선택
+//   const newMessage = document.createElement("div"); // 새로운 div 요소 생성
+//   const newMessage2 = document.createElement("span"); // 새로운 span 요소 생성
+//   newMessage.classList.add("chat-wrap", "my-chat-text"); // 클래스 추가
+//   newMessage2.classList.add("chat-box", "my-chat");
+//   // newMessage.textContent = message; // 메세지 텍스트 추가
+//   newMessage2.textContent = message;
+//   newMessage.appendChild(newMessage2);
+//   chatBox.appendChild(newMessage); // 채팅 박스에 새 메세지 요소 추가
+//   // chatBox.appendChild(newMessage2);
+//   document.querySelector("#message").value = ""; // 메세지 입력값 초기화
+// }
+
+// 클라이언트별 이미지 URL 매핑 객체
+const clientImageUrls = new Map();
+
+const imageUrls = [
+  "/views/quokka1.png",
+  "/views/quokka2.png",
+  "/views/quokka3.png",
+];
+
+// 채팅창 메세지 전송 Step1
+// "send" 이벤트 전송 { 닉네임, 입력메세지 }
+function send() {
+  const data = {
+    myNick: myNick,
+    msg: document.querySelector("#message").value,
+    imageUrl: getRandomImageUrl(socket.id),
+  };
+  socket.emit("send", data);
+
+  document.querySelector("#message").value = ""; // 인풋 초기화
+}
+
+// 랜덤 이미지 URL 반환하는 함수
+function getRandomImageUrl() {
+  const randomIndex = Math.floor(Math.random() * imageUrls.length);
+  return imageUrls[randomIndex];
+}
+
+// 이미지 URL 사용 예시
+const imageUrl = getRandomImageUrl();
+console.log("이미지 URL: ", imageUrl);
+
+// 채팅창 메세지 전송 Step2
+// 서버에 접속한 모든 클라이언트한테 "누가 뭐라고 말했는지" 이벤트 보내기
+socket.on("newMessage", (data) => {
+  console.log("socket on newMessage >> ", data);
+  const chatBox = document.querySelector("#chatBox"); // 채팅 박스 요소 선택
+  const newMessage = document.createElement("div"); // 새로운 div 요소 생성
+  const newMessage2 = document.createElement("span"); // 새로운 span 요소 생성
+
+  // newMessage2.innerText = data.msg;
+
+  if (data.nick === myNick) {
+    newMessage.classList.add("chat-wrap", "my-chat-text"); // 클래스 추가
+    newMessage2.classList.add("chat-box", "my-chat");
+    newMessage2.innerText = data.msg;
+  } else {
+    newMessage.classList.add("chat-wrap", "other-chat-text"); // 클래스 추가
+    newMessage2.classList.add("chat-box", "other-chat");
+    newMessage2.innerText = data.nick + ": " + data.msg;
+  }
+
+  newMessage.appendChild(newMessage2);
+  chatBox.appendChild(newMessage); // 채팅 박스에 새 메세지 요소 추가
+
+  // 랜덤 이미지 선택
+  if (data.nick !== myNick) {
+    const randomImageUrl = data.imageUrl;
+    const imageElement = document.createElement("img");
+    const imageElement1 = document.createElement("img");
+
+    imageElement.classList.add("chat-wrap", "other-chat-text"); // 클래스 추가
+    imageElement1.classList.add("chat-box", "other-chat");
+    // imageElement1.src = randomImageUrl;
+    imageElement.src = randomImageUrl;
+
+    // 이미지의 위치 조정
+    imageElement.style.position = "absolute";
+    imageElement.style.top = "-0.5rem";
+    imageElement.style.left = "-2.2rem";
+    imageElement.style.width = "3rem";
+    imageElement.style.height = "3rem";
+    // 이미지를 원형으로 표시
+    imageElement.style.borderRadius = "50%";
+    newMessage.appendChild(imageElement);
+  }
+});
+
+// // 이미지 랜덤
+// const randomImageUrl =
+//     imageUrls[Math.floor(Math.random() * imageUrls.length)];
+// const imageElement = document.createElement("img");
+// const imageElement1 = document.createElement("img");
+//
+// imageElement.classList.add("chat-wrap", "other-chat-text"); // 클래스 추가
+// imageElement1.classList.add("chat-box", "other-chat");
+// imageElement.src = randomImageUrl;
+// // imageElement1.src = randomImageUrl;
+//
+// // imageElement.appendChild(imageElement1);
+// newMessage.appendChild(imageElement);
+// }
