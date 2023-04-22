2da3e9ec037a9267d713d7a307f21cd7a2b8bd53
8566yu <8566yu@gmail.com>
2023-04-22 15:08:48 +0900

    0422 react_ref

---
+++ 0422(react_ref)/0422_react_ref/src/App.js
@@ -0,0 +1,20 @@
+import RefSample1 from "./RefSample1";
+import RefSample2 from "./RefSample2";
+import RefSample3 from "./RefSample3";
+import RefSample4 from "./RefSample4";
+import Ex from "./Ex";
+
+function App() {
+  return (
+    <div className="App">
+      <RefSample1 />
+      <RefSample2 />
+      <RefSample3 />
+      <RefSample4 />
+      <hr />
+      <Ex />
+    </div>
+  );
+}
+
+export default App;
