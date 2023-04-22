53c952a6b9ade7cca03b886bae12a04e9ad6ab73
8566yu <8566yu@gmail.com>
2023-04-22 16:26:29 +0900

    0422 react_life

---
+++ 0422(react_lifeCycle)/0422_react_lifeCycle/src/App.js
@@ -0,0 +1,14 @@
+import LifeCycleFunc from "./LifeCycleFunc";
+import LifeCycleClass from "./LifeCycleClass";
+function App() {
+  return (
+    <div className="App">
+      <LifeCycleFunc />
+      <hr />
+      <hr />
+      <LifeCycleClass />
+    </div>
+  );
+}
+
+export default App;
---
+++ 0422(react_lifeCycle)/0422_react_lifeCycle/src/LifeCycleFuncChild.js
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { useState } from "react";
+// 자식 컴포넌트
+const LifeCycleFuncChild = (props) => {
+  // console.log("props >>> ", props);
+  const { number } = props;
+  const [text, setText] = useState("");
+
+  // useEffect(() => {
+  //   // mount 시점에 실행
+  //   console.log("mount!!!");
+  //
+  //   // unmount 시점에 실행
+  //   return () => {
+  //     console.log("unmount!!!");
+  //   };
+  // }, []);
+  //
+  // // mount & update 시점에 실행
+  // useEffect(() => {
+  //   console.log("mount or update!!!");
+  // });
+
+  // text state 가 바뀔떄마다 실행
+  useEffect(() => {
+    console.log("update!!!");
+  }, [text]);
+
+  return (
+    <>
+      <p>
+        LifeCycleFuncChild <b>{number}</b>{" "}
+      </p>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </>
+  );
+};
+
+export default LifeCycleFuncChild;
