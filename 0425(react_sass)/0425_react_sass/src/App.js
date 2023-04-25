0acc8978f4422e49a13e63055df8bca1a8f49366
8566yu <8566yu@gmail.com>
2023-04-25 20:02:48 +0900

    0425 react_css_sass

---
+++ 0425(react_sass)/0425_react_sass/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import CssComponent from "./CssComponent";
+// import SassComponent from "./SassComponent";
+import Lava from "./Lava";
+
+function App() {
+  return (
+    <div className="App">
+      {/*<CssComponent />*/}
+      {/*<SassComponent />*/}
+      <Lava />
+    </div>
+  );
+}
+
+export default App;
---
+++ 0425(react_sass)/0425_react_sass/src/Lava.js
@@ -0,0 +1,27 @@
+import React from "react";
+// import "./style/Lava.scss";
+// import image from "./style/grass.png";
+import "./style/Colorss.scss";
+const Lava = () => {
+  return (
+    // <div className="container">
+    //   <div className="circle circle1"></div>
+    //   <div className="circle circle2"></div>
+    //   <div className="circle circle3"></div>
+    //   <div className="circle circle4"></div>
+    //   <div className="circle circle5"></div>
+    //   <div className="circle circle6"></div>
+    //   <div className="circle circle7"></div>
+    //   <div className="gr">
+    //     <img src={image} alt="사진" />
+    //   </div>
+
+    <div className="SassComponent">
+      <div className="box pink"></div>
+      <div className="box yellow"></div>
+      <div className="box green"></div>
+    </div>
+    // </div>
+  );
+};
+export default Lava;
